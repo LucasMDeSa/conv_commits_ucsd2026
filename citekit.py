d8c0364df52f512bbecd2a7ccd4fc0f161fb519c
Lucas de Sá <lucas.marquesanto@gmail.com>
2026-04-10 19:34:55 -0700

    feat!: restructure author fields to support multiple authors
    Replace the flat 'author_last'/'author_first' keys with an 'authors'
    list of {'last', 'first'} dicts. All three formatters updated to handle
    one, two, or more authors per style guide rules. BibTeX entries now
    render multi-author fields with the ' and ' separator per BibTeX convention.
    
    BREAKING CHANGE: 'author_last' and 'author_first' keys are removed.
    Callers must migrate to authors=[{"last": ..., "first": ...}].

--- citekit.py
+++ citekit.py
@@ -5,11 +5,20 @@ def format_apa(ref):
     """Format a reference dict in APA 7th edition style.
 
     Args:
-        ref: dict with keys 'author_last', 'author_first', 'year', 'title',
-             and optionally 'journal'.
+        ref: dict with keys 'authors' (list of {'last': str, 'first': str}),
+             'year', 'title', and optionally 'journal'.
     """
-    author = f"{ref['author_last']}, {ref['author_first'][0]}."
-    citation = f"{author} ({ref['year']}). {ref['title']}."
+    authors = ref["authors"]
+    if len(authors) == 1:
+        a = authors[0]
+        author_str = f"{a['last']}, {a['first'][0]}."
+    elif len(authors) == 2:
+        a, b = authors
+        author_str = f"{a['last']}, {a['first'][0]}., & {b['last']}, {b['first'][0]}."
+    else:
+        a = authors[0]
+        author_str = f"{a['last']}, {a['first'][0]}., et al."
+    citation = f"{author_str} ({ref['year']}). {ref['title']}."
     if "journal" in ref:
         citation += f" {ref['journal']}."
     return citation
@@ -19,11 +28,19 @@ def format_mla(ref):
     """Format a reference dict in MLA 9th edition style.
 
     Args:
-        ref: dict with keys 'author_last', 'author_first', 'year', 'title',
-             and optionally 'journal'.
+        ref: dict with keys 'authors' (list of {'last': str, 'first': str}),
+             'year', 'title', and optionally 'journal'.
     """
-    author = f"{ref['author_last']}, {ref['author_first']}"
-    citation = f'{author}. "{ref["title"]}."'
+    authors = ref["authors"]
+    if len(authors) == 1:
+        author_str = f"{authors[0]['last']}, {authors[0]['first']}"
+    elif len(authors) == 2:
+        a, b = authors
+        author_str = f"{a['last']}, {a['first']}, and {b['first']} {b['last']}"
+    else:
+        a = authors[0]
+        author_str = f"{a['last']}, {a['first']}, et al."
+    citation = f'{author_str}. "{ref["title"]}."'
     if "journal" in ref:
         citation += f" {ref['journal']},"
     citation += f" {ref['year']}."
@@ -34,12 +51,15 @@ def format_bibtex(ref):
     """Format a reference dict as a BibTeX @article entry.
 
     Args:
-        ref: dict with keys 'author_last', 'author_first', 'year', 'title',
-             and optionally 'journal'.
+        ref: dict with keys 'authors' (list of {'last': str, 'first': str}),
+             'year', 'title', and optionally 'journal', 'volume', 'number', 'pages'.
     """
-    key = f"{ref['author_last'].lower().replace(' ', '')}{ref['year']}"
+    key = f"{ref['authors'][0]['last'].lower().replace(' ', '')}{ref['year']}"
+    author_field = " and ".join(
+        f"{a['last']}, {a['first']}" for a in ref["authors"]
+    )
     entry = f"@article{{{key},\n"
-    entry += f"  author  = {{{ref['author_last']}, {ref['author_first']}}},\n"
+    entry += f"  author  = {{{author_field}}},\n"
     entry += f"  year    = {{{ref['year']}}},\n"
     entry += f"  title   = {{{ref['title']}}}"
     for field in ("journal", "volume", "number", "pages"):
